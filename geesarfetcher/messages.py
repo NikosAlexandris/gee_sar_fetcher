2136cf16ffd3408f0338fc531167d0aa175537bd
Nikos Alexandris <nik@nikosalexandris.net>
2020-11-18 00:08:38 +0100

    [SquashMe] Add messages.py

---
+++ geesarfetcher/messages.py
@@ -0,0 +1,10 @@
+MESSAGE_NO_BANDS_IN_COLLECTION = 'ImageCollection.getRegion: No bands in collection.'
+VALUE_ERROR_NO_BANDS_IN_COLLECTION = (
+        'No bands found in collection. '
+        'Orbit incorrect for localisation, please visit '
+        'https://sentinel.esa.int/web/sentinel/missions/sentinel-1/observation-scenario '
+        'for more info.'
+)
+VALUE_ERROR_NO_COORDINATES = (
+        'coordinates must be None if top_left and bottom_right are not None.'
+)
