2140f8acd270ebfe040cf252d66c46e56902709e
Nikos Alexandris <nik@nikosalexandris.net>
2020-11-18 12:38:50 +0100

    Add compose_sentinel1_data()

--- geesarfetcher/compose.py
+++ geesarfetcher/compose.py
@@ -1,3 +1,103 @@
+def compose_sentinel1_data(
+        start_date,
+        end_date,
+        geometry,
+        scale,
+        crs,
+        pass_direction=ASCENDING,
+        statistic=MEAN,
+    ):
+    '''
+    Composes an image from an ImageCollection using input parameters and return data as a tuple of header and values.
+
+    Parameters
+    ----------
+    start_date : str
+        str following the pattern ``'yyyy-mm-dd'`` describing the start date of
+        the time interval
+
+    end_date : str
+        str following the pattern ``'yyyy-mm-dd'`` describing the end date of
+        the time interval
+
+    geometry : ee.Geometry
+        Geometry object defining the area of process
+
+    scale : int
+        Scale parameters of the getRegion() function. Defaulting at ``20``,
+        change it to change the scale of the final data points. The highest,
+        the lower the spatial resolution. Should be at least ``10``.
+
+    pass_direction : str, optional
+        Defines the pass direction to set for the data retrieval process
+
+    Returns
+    -------
+    (val_header, val) : tuple
+        val_header corresponds to the ``list of str`` describing the fields of
+        the val array. The val array is a ``list`` of data records, each
+        represented as a ``list`` of the same size as the val_header array.
+    '''
+    filtered_sentinel1_data = filter_sentinel1_data(
+            start_date=start_date,
+            end_date=end_date,
+            geometry=geometry,
+            pass_direction=pass_direction
+    )
+    values_vv = (filtered_sentinel1_data
+              .select(VV)
+              .reduce(statistic)
+              .sample(
+                  region=geometry,
+                  scale=scale,
+                  projection=crs,
+                  geometries=True,
+                  dropNulls=False,
+              )
+              .getInfo()
+    )
+    values_vh = (filtered_sentinel1_data
+              .select(VH)
+              .reduce(statistic)
+              .sample(
+                  region=geometry,
+                  scale=scale,
+                  projection=crs,
+                  geometries=True,
+                  dropNulls=False,
+              )
+              .getInfo()
+    )
+    header = ([
+                'longitude',
+                'latitude',
+                'start_date',
+                'end_date',
+                VV,
+                VH]
+    )
+    vv = []
+    for feature in values_vv['features']:
+        coordinates = feature['geometry']['coordinates']
+        feature_property = feature['properties']
+        value_vv = feature_property.get(VV + '_' + statistic)
+        vv.extend([coordinates + [value_vv]])
+    vh = []
+    for feature in values_vh['features']:
+        coordinates = feature['geometry']['coordinates']
+        feature_property = feature['properties']
+        value_vh = feature_property.get(VH + '_' + statistic)
+        vh.extend([coordinates + [value_vh]])
+        vh.extend([value_vh])
+    values = [
+        vv[idx] +
+        [start_date] +
+        [end_date] +
+        [vh[idx]]
+        for idx in range(len(vv))
+    ]
+    return (header, values)
+
 def compose(
     top_left=None,
     bottom_right=None,
