6c285ebb008d77b580c01f38dacb72eb4672f812
Nikos Alexandris <nik@nikosalexandris.net>
2020-11-19 16:56:51 +0100

    Add/update populate_coordinates_dictionary()

--- geesarfetcher/coordinates.py
+++ geesarfetcher/coordinates.py
@@ -21,19 +21,21 @@ def latitudes_and_longitudes(pixel_values):
     return unique_latitudes, unique_longitudes
 
 
+def populate_coordinates_dictionary(dictified_values):
+    """Build a custom dictionary 'coordinates_dictionary' populated with values from
+    the 'dictified_values' dictionnary
 
     Parameters
     ----------
     dictified_values :
         A dictionary of Sentinel-1 values
 
+    Returns
+    -------
     coordinates_dictionary :
         A dictionnary matching to each coordinate key its values through time
         as well as its timestamps.
 
-    Returns
-    -------
-    coordinates_dictionary
     """
     coordinates_dictionary = {}
     for entry in dictified_values:
