94467f57974799a4b9f0ffcb9bc65787cb36ca69
Nikos Alexandris <nik@nikosalexandris.net>
2020-11-20 01:05:36 +0100

    Replace 'test_compose_function.py' with two other example scripts

--- geesarfetcher/example_compose_s1_grd_data_over_srorg6974.py
+++ geesarfetcher/example_compose_s1_grd_data_over_srorg6974.py
@@ -1,4 +1,5 @@
 # imports
+import os
 import ee
 ee.Initialize()
 import datetime
@@ -17,21 +18,21 @@ SRORG6974 = 'SR-ORG:6974'  # projection
 
 # where?
 scale=1000
-top_left = [-104.77431630331856, 41.829889598264826]
-bottom_right = [-104.65140675742012, 41.81515375846025]
+# top_left = [-104.77431630331856, 41.829889598264826]
+# bottom_right = [-104.65140675742012, 41.81515375846025]
 # top_left = [5.26948161, 49.97440374]
-# bottom_right = [8.07913236, 47.90621773]
+top_left = [7.26948161, 49.97440374]
+bottom_right = [8.07913236, 48.90621773]
 x_min = top_left[0]
 y_min = bottom_right[1]
 x_max = bottom_right[0]
 y_max = top_left[1]
 extent = (x_min, y_min, x_max, y_max)
-# geometry = ee.Geometry.Rectangle(extent, SRORG6974, False)
-geometry = ee.Geometry.Rectangle(extent)
+geometry = ee.Geometry.Rectangle(extent, SRORG6974, False)
 
 # when?
 start_date = datetime.datetime(2019, 6, 1)
-end_date = datetime.datetime(2019, 6, 10)
+end_date = datetime.datetime(2019, 6, 30)
 
 # compose
 statistic = 'mean'
@@ -44,6 +45,7 @@ sentinel_1_composite = compose(
     scale=scale,
     crs=SRORG6974,
     statistic=statistic,
+    n_jobs=30,
 )
 
 # write out
@@ -53,8 +55,9 @@ output = numpy.dstack((
     sentinel_1_composite['stack']
     )
 )
+filename = os.path.basename(__file__)
 savetxt(
-        fname='s1_grd.csv',
+        fname=f'{filename}.csv',
         X=output[0],
         fmt='%.18f',
         delimiter=',',
