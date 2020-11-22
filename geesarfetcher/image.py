13f0316faa3af35405d279a6e006bfd8842c5285
Nikos Alexandris <nik@nikosalexandris.net>
2020-11-22 06:28:48 +0100

    Update image.py for use of northing(s) and easting(s)
    - Add import northing_and_easting
    - Use northing_and_easting()
    - Rename paramters of generate_image()

--- geesarfetcher/image.py
+++ geesarfetcher/image.py
@@ -2,30 +2,32 @@ import numpy
 from tqdm import tqdm
 from .constants import VV
 from .constants import VH
+from .coordinates import northing_and_easting
 
 
 def generate_image(
         height,
         width,
         pixel_values,
-        unique_latitudes,
-        unique_longitudes,
+        unique_northings,
+        unique_eastings,
     ):
     """
     """
-    latitudes_dictionary = {
-            unique_latitudes[i]: i
-            for i in range(len(unique_latitudes))
+    northings_dictionary = {
+            unique_northings[i]: i
+            for i in range(len(unique_northings))
     }
-    longitudes_dictionary = {
-            unique_longitudes[i]: i
-            for i in range(len(unique_longitudes))
+    eastings_dictionary = {
+            unique_eastings[i]: i
+            for i in range(len(unique_eastings))
     }
     image = numpy.full((height, width, 2), fill_value=numpy.nan)
     print(f'Generating image of shape (height x width) {height, width}')
     for pixel_value in tqdm(pixel_values):
-        x = latitudes_dictionary[pixel_value['lat']]
-        y = longitudes_dictionary[pixel_value['lon']]
+        northing, easting = northing_and_easting(pixel_value)
+        x = northings_dictionary[pixel_value[northing]]
+        y = eastings_dictionary[pixel_value[easting]]
         image[x, y, 0] = numpy.nanmean(pixel_value[VV], dtype=float)
         image[x, y, 1] = numpy.nanmean(pixel_value[VH], dtype=float)
     return image
