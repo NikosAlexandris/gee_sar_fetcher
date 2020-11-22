3edda4c1bfb98dfeae290187c6b45fa3cd135de5
Nikos Alexandris <nik@nikosalexandris.net>
2020-11-22 06:55:25 +0100

    Update test script example_compose_s1_grd_data_over_epsg4326.py

--- geesarfetcher/example_compose_s1_grd_data_over_epsg4326.py
+++ geesarfetcher/example_compose_s1_grd_data_over_epsg4326.py
@@ -1,48 +1,19 @@
-# imports
-import os
 import ee
 ee.Initialize()
 import datetime
 from geesarfetcher.api import compose
 import numpy
+import os
 from numpy import savetxt
 
-# constants
-SENTINEL1_COLLECTION_ID = 'COPERNICUS/S1_GRD'
-VV = 'VV'
-VH = 'VH'
-IW = 'IW'
-ASCENDING = 'ASCENDING'
-DESCENDING = 'DESCENDING'
-
-# where?
-scale=1000
-# top_left = [-104.77431630331856, 41.829889598264826]
-# bottom_right = [-104.65140675742012, 41.81515375846025]
-# top_left = [5.26948161, 49.97440374]
-top_left = [7.26948161, 49.97440374]
-bottom_right = [8.07913236, 48.90621773]
-x_min = top_left[0]
-y_min = bottom_right[1]
-x_max = bottom_right[0]
-y_max = top_left[1]
-extent = (x_min, y_min, x_max, y_max)
-geometry = ee.Geometry.Rectangle(extent)
-
-# when?
-start_date = datetime.datetime(2019, 6, 1)
-end_date = datetime.datetime(2019, 6, 30)
-
-# compose
-statistic = 'mean'
 sentinel_1_composite = compose(
-    top_left=top_left,
-    bottom_right=bottom_right,
-    start_date=start_date,
-    end_date=end_date,
+    top_left=[7.26948161, 49.97440374],
+    bottom_right=[8.07913236, 48.90621773],
+    start_date=datetime.datetime(2019, 6, 1),
+    end_date=datetime.datetime(2019, 6, 30),
     ascending=True,
-    scale=scale,
-    statistic=statistic,
+    scale=1000,
+    statistic='mean',
     n_jobs=30,
 )
 
