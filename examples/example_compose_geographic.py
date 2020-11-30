0ebadc4742bf94676c51a64b52a9dd168614daf4
Nikos Alexandris <nik@nikosalexandris.net>
2020-11-30 22:14:23 +0100

    Update example_compose_geographic.py

--- examples/example_compose_geographic.py
+++ examples/example_compose_geographic.py
@@ -1,35 +1,44 @@
+from devtools import debug
 import ee
 ee.Initialize()
 import datetime
 from geesarfetcher.api import compose
-import numpy
-import os
-from numpy import savetxt
+from geesarfetcher.api import write_to_csv
+from where_when_what import image_collection
 
-sentinel_1_composite = compose(
-    top_left=[7.26948161, 49.97440374],
-    bottom_right=[8.07913236, 48.90621773],
-    start_date=datetime.datetime(2019, 6, 1),
-    end_date=datetime.datetime(2019, 6, 30),
+n=49.9695488705556
+s=47.9108967116667
+w=5.25413596111111
+e=8.07845648166667
+
+top_left = [w, n]
+bottom_right = [e, s]
+location = (top_left, bottom_right)
+
+start_date = datetime.datetime(2019, 6, 1)
+end_date = datetime.datetime(2019, 6, 6)
+interval = (start_date, end_date)
+
+#
+statistic = 'mean'
+composite = compose(
+    top_left=top_left,
+    bottom_right=bottom_right,
+    start_date=start_date,
+    end_date=end_date,
     ascending=True,
     scale=1000,
-    statistic='mean',
+    statistic=statistic,
     n_jobs=30,
 )
 
 # write out
-header = (',').join(['Latitude,Longitude,VV,VH'])
-output = numpy.dstack((
-    sentinel_1_composite['coordinates'],
-    sentinel_1_composite['stack']
-    )
-)
-filename = os.path.basename(__file__)
-savetxt(
-        fname=f'{filename}.csv',
-        X=output[0],
-        fmt='%.18f',
-        delimiter=',',
-        header=header,
-        comments=''
+debug(locals())
+write_to_csv(
+        composite_data=composite,
+        image_collection=image_collection,
+        location=location,
+        interval=interval,
+        statistic=statistic,
+        structured=False,
 )
