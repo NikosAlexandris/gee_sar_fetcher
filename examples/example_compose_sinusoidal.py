4a38bdb5b6d03f56a79c198460bfd7b737cafcaf
Nikos Alexandris <nik@nikosalexandris.net>
2020-11-30 22:19:31 +0100

    Update example_compose_sinusoidal.py

--- examples/example_compose_sinusoidal.py
+++ examples/example_compose_sinusoidal.py
@@ -1,4 +1,3 @@
-# imports
 import ee
 ee.Initialize()
 import datetime
@@ -7,39 +6,36 @@ from geesarfetcher.api import compose
 from geesarfetcher.api import write_to_csv
 from where_when_what import image_collection
 
-# constants
-SENTINEL1_COLLECTION_ID = 'COPERNICUS/S1_GRD'
-VV = 'VV'
-VH = 'VH'
-IW = 'IW'
-ASCENDING = 'ASCENDING'
-DESCENDING = 'DESCENDING'
-SRORG6974 = 'SR-ORG:6974'  # projection
-
 # where?
 # g.region \
 #        vect=region_modis_landsat8_ecostress@PERMANENT -p
 # nsres=926.76896281 ewres=924.94989462 -pelag
 # projection=99
 # zone=0
-# n=5539298.09071537
-# s=5307605.85001287
-# w=378304.50689958
-# e=603992.28118686
+n=5539298.09071537
+s=5307605.85001287
+w=378304.50689958
+e=603992.28118686
+top_left = [w, n]
+bottom_right = [e, s]
 nsres=926.76896281
 # ewres=924.94989462
 # rows=250
 # cols=244
 # cells=61000
 #-
-nw_long=7.27500244
-nw_lat=49.98607421
+###
+# nw_long=7.27500244
+# nw_lat=49.98607421
+###
 #-
 # ne_long=8.42194767
 # ne_lat=49.98607421
 #-
-se_long=8.07847752
-se_lat=48.90267285
+###
+# se_long=8.07847752
+# se_lat=48.90267285
+###
 #-
 # sw_long=5.05987336
 # sw_lat=47.90267285
@@ -47,14 +43,16 @@ se_lat=48.90267285
 # center_lat=48.94446807
 # ns_extent=231692.240703
 # ew_extent=225687.774287
-left, top, right, bottom = degrees_to_sinusoidal(
-        west = nw_long,
-        north = nw_lat,
-        east = se_long,
-        south = se_lat,
-)
-top_left = [left, top]
-bottom_right = [right, bottom]
+###
+# left, top, right, bottom = degrees_to_sinusoidal(
+#         west = nw_long,
+#         north = nw_lat,
+#         east = se_long,
+#         south = se_lat,
+# )
+# top_left = [left, top]
+# bottom_right = [right, bottom]
+###
 location = (top_left, bottom_right)
 #-
 #extent = (left, bottom, right, top)
